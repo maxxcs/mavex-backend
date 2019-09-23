2db3ef6b9de247f5df04a39b6a46f9514ff32b27
Max <max.carlesso@hotmail.com>
2019-09-22 21:56:41 -0300

    WIP authentication

---
+++ src/config/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const SECRET = 'E234ASER423S4E234S23E4S23D4F23S423R42T3432GF4D23H4T324D'; // Very secret, so much protection
+
+function sign(payload) {
+  return new Promise((resolve, reject) => {
+    jwt.sign(payload, SECRET, { algorithm: 'HS256' }, (err, token) => {
+      if (err) return reject({ message: 'Something unexpected has occurred.' });
+      return resolve(token);
+    });
+  });
+}
+
+function verify(token) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, SECRETE, { algorithm: 'HS256' }, (err, decoded) => {
+      if (err) return reject({ message: 'Something unexpected has occurred.' });
+      return resolve(decoded);
+    });
+  });
+}
+
+module.exports = { sign, verify };
--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,4 +1,5 @@
 const UserModel = require('../models/user');
+const { sign, verify } = require('../config/auth');
 
 const home = async (fast, opts, done) => {
   fast.get('/', async (request, reply) => {
@@ -16,7 +17,6 @@ const home = async (fast, opts, done) => {
   fast.post('/login', async (request, reply) => {
     try {
       reply.type('application/json');
-      console.log(request.body);
       const user = await UserModel.findOne({ username: request.body.username }).select('+password');
       if (!user) {
         throw new Error('Username or password is invalid.');
@@ -24,11 +24,14 @@ const home = async (fast, opts, done) => {
         if (!await user.comparePassword(request.body.password)) {
           throw new Error('Username or password is invalid.');
         } else {
-          user.password = undefined;
-          return user;
+          const token = await sign({
+            username: user.username,
+            email: user.email,
+            permission: user.level
+          });
+          return { token };
         }
       }
-
     } catch (err) {
       reply.type('application/json');
       reply.code(400);
@@ -57,18 +60,6 @@ const home = async (fast, opts, done) => {
       reply.code(400);
       console.log(err.message);
       return { message: err.message };
-    }
-  });
-
-  fast.post('/logout', async (request, reply) => {
-    try {
-      reply.type('application/json');
-      return { controller: 'home', location: 'logout' };
-
-    } catch (err) {
-      reply.type('application/json');
-      console.log(err);
-      return err;
     }
   });
 
